cb349cd8de9cacf5b4b2d69fa70ba05a5bb26e2b
mitAakash <122879494+mitAakash@users.noreply.github.com>
2025-10-27 12:49:20 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { Store } from './App/Store'; // 👈 adjust this path based on your project
+import { Store } from './App/Store'; 
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Increment button', () => {
   render(
     <Provider store={Store}>
       <App />
     </Provider>
   );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole('button', { name: /increment/i });
+  expect(buttonElement).toBeInTheDocument();
 });
